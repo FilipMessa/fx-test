3a79bfea2c9c1a3623014599d04b1770f517a8e9
Filip Messa <messa.filip@gmail.com>
2022-01-10 15:39:40 +0100

    Setup the application routing
    - install the react-router-dom package v.6.2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Button } from "antd";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Button type="primary">Button ANT Design</Button>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Routes>
   </div>
 );
 
---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export const About = () => (
+  <>
+    <main>
+      <h2>Who are we?</h2>
+      <p>That feels like an existential question, don't you think?</p>
+    </main>
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+  </>
+);
