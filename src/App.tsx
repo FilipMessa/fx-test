069dba9336a23e0dc6e10725468d420143e51e41
Filip Messa <messa.filip@gmail.com>
2022-01-10 23:44:15 +0100

    Create ErrorBoundary component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Suspense, lazy } from "react";
 import { Spin } from "antd";
 import "./App.css";
 
+import { ErrorBoundary } from "./components/ErrorBoundary";
+
 const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 
 const App = () => (
-  <div className="App">
+  <ErrorBoundary>
     <Button type="primary">Button ANT Design</Button>
     <Suspense fallback={<Spin tip="Loading..." />}>
       <Routes>
@@ -16,7 +18,7 @@ const App = () => (
         <Route path="about" element={<About />} />
       </Routes>
     </Suspense>
-  </div>
+  </ErrorBoundary>
 );
 
 export default App;
---
+++ src/services/logger.tsx
@@ -0,0 +1,2 @@
+// TODO implement logger
+export default console.error;
